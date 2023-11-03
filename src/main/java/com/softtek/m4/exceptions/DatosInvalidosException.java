d9caf24b0fe659459415670c2dff3d797e23444f
Nicolás Muros <nicomuros@gmail.com>
2023-11-03 16:59:42 -0300

    Excepción personalizada para datos invalidos

---
+++ src/main/java/com/softtek/m4/exceptions/DatosInvalidosException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.softtek.m4.exceptions;
+
+/**
+ *
+ * @author nicom
+ */
+public class DatosInvalidosException extends RuntimeException{
+    public DatosInvalidosException(String message){
+        super(message);
+    }
+}
