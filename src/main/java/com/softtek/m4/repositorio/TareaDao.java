fda2b5dde5cdc40214fec33a48e86925ffdfa8ca
Nicolás Muros <nicomuros@gmail.com>
2023-11-05 14:35:23 -0300

    TareaDao Dcoumentado

--- src/main/java/com/softtek/m4/repositorio/TareaDao.java
+++ src/main/java/com/softtek/m4/repositorio/TareaDao.java
@@ -4,12 +4,47 @@ import com.softtek.m4.exceptions.NonexistentEntityException;
 import com.softtek.m4.modelo.entidades.Tarea;
 import java.util.List;
 
+/**
+ * Interfaz que define operaciones de acceso a datos para las entidades de Tarea.
+ */
 public interface TareaDao {
+
+    /**
+     * Crea una nueva Tarea en el repositorio.
+     *
+     * @param tarea La Tarea a ser creada.
+     */
     void create(Tarea tarea);
+
+    /**
+     * Edita una Tarea existente en el repositorio.
+     *
+     * @param tarea La Tarea a ser modificada.
+     * @throws NonexistentEntityException Si la Tarea no existe en el repositorio.
+     * @throws Exception Si ocurre algún error durante la edición.
+     */
     void edit(Tarea tarea) throws NonexistentEntityException, Exception;
+
+    /**
+     * Elimina una Tarea del repositorio mediante su identificador.
+     *
+     * @param id El identificador de la Tarea a ser eliminada.
+     * @throws NonexistentEntityException Si la Tarea no existe en el repositorio.
+     */
     void destroy(Integer id) throws NonexistentEntityException;
+
+    /**
+     * Obtiene una lista de todas las Tareas existentes en el repositorio.
+     *
+     * @return Una lista de objetos Tarea.
+     */
     List<Tarea> findTareaEntities();
-    List<Tarea> findTareaEntities(int maxResults, int firstResult);
+
+    /**
+     * Busca una Tarea específica por su identificador.
+     *
+     * @param id El identificador de la Tarea a buscar.
+     * @return La Tarea encontrada o null si no existe.
+     */
     Tarea findTarea(Integer id);
-    int getTareaCount();
 }
